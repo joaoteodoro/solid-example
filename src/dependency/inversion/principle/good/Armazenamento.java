e57a99431723d4d123945bac25887df0a1c4ca73
João Neto DECSKILL <JoaoNeto.DECSKILL@edp.com>
2024-03-26 07:51:16 -0300

    ajust

--- src/dependency/inversion/principle/good/Armazenamento.java
+++ src/dependency/inversion/principle/good/Armazenamento.java
@@ -3,7 +3,7 @@ package dependency.inversion.principle.good;
 /**
  * Na versão corrigida, a classe Calculadora não depende mais diretamente do banco de dados. Em vez disso, ela depende
  * da interface Armazenamento. Isso torna a classe mais flexivel e permite que ela seja usada com diferentes tipos de
- * armazenamento.
+ * armazenamento .
  */
 public interface Armazenamento {
     void salvar(int resultado);
